64ff59bb03c5578f1bb3d36fc76ad82d9ad9cbd4
Alan Smillie <alan.smillie@gmail.com>
2019-12-11 14:49:15 -0500

    Create Config Service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { ConfigService } from './config/config.service';
+import { ConfigModule } from './config/config.module';
+
+const typeOrmConfig = {
+  type: 'mongodb',
+  host: 'localhost',
+  port: '',
+  username: '',
+  password: '',
+  database: '',
+  entities: [],
+  synchronize: true,
+};
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, ConfigModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ConfigService],
 })
 export class AppModule {}
