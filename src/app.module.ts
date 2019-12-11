182e8c5f088a0776714cbfec331918662591b7fb
Alan Smillie <alan.smillie@gmail.com>
2019-12-11 15:33:43 -0500

    Remove TypeOrm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -7,19 +6,11 @@ import { UsersModule } from './users/users.module';
 import { ConfigService } from './config/config.service';
 import { ConfigModule } from './config/config.module';
 
-const typeOrmConfig = {
-  type: 'mongodb',
-  host: 'localhost',
-  port: '',
-  username: '',
-  password: '',
-  database: '',
-  entities: [],
-  synchronize: true,
-};
-
 @Module({
-  imports: [UsersModule, ConfigModule],
+  imports: [
+    UsersModule,
+    ConfigModule,
+  ],
   controllers: [AppController],
   providers: [AppService, ConfigService],
 })
