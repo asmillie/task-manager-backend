f8873ab612dc9d07a2d46025806443b41c5f4df2
Alan Smillie <alan.smillie@gmail.com>
2019-12-11 15:29:41 -0500

    Add getters for config vars

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -1,15 +1,38 @@
 import { Injectable } from '@nestjs/common';
 import * as dotenv from 'dotenv';
 import * as fs from 'fs';
+import { numberLiteralTypeAnnotation } from '@babel/types';
 
 export class ConfigService {
     private readonly envConfig: Record<string, string>;
+
+    private readonly PORT = 'PORT';
+    private readonly JWT_SECRET = 'JWT_SECRET';
+    private readonly DB_HOST = 'DB_HOST';
+    private readonly DB_PORT = 'DB_PORT';
+    private readonly DB_NAME = 'DB_NAME';
 
     constructor(filePath: string) {
         this.envConfig = dotenv.parse(fs.readFileSync(filePath));
     }
 
-    get(key: string): string {
-        return this.envConfig[key];
+    get port(): string {
+        return this.envConfig.PORT;
+    }
+
+    get jwt_secret(): string {
+        return this.envConfig.JWT_SECRET;
+    }
+
+    get databaseHost(): string {
+        return this.envConfig.DB_HOST;
+    }
+
+    get databasePort(): string {
+        return this.envConfig.DB_PORT;
+    }
+
+    get databaseName(): string {
+        return this.envConfig.DB_NAME;
     }
 }
