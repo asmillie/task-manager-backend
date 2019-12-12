81314476691caef5f9ed80950b1bc27cfee94417
Alan Smillie <alan.smillie@gmail.com>
2019-12-12 10:34:18 -0500

    Add create and find methods

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model, ObjectId } from 'mongoose';
+import { User } from './interfaces/user.interface';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(@InjectModel('User') private readonly userModel: Model<User>) {}
+
+    async create(createUserDto: CreateUserDto): Promise<User> {
+        const createdUser = new this.userModel(createUserDto);
+        return await createdUser.save();
+    }
+
+    async findAll(): Promise<User> {
+        return await this.userModel.find().exec();
+    }
+}
