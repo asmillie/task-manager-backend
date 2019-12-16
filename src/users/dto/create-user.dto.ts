b01508e15fdf1c04809f3df3584a8782a8f92aeb
Alan Smillie <alan.smillie@gmail.com>
2019-12-16 15:20:50 -0500

    Move validation from schema to dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,8 +1,17 @@
+import { IsNotEmpty, MinLength, IsEmail, IsLowercase } from 'class-validator';
+
 export class CreateUserDto {
+    @IsNotEmpty({ message: 'User name is required' })
     readonly name: string;
+
+    @IsNotEmpty({ message: 'Password is required '})
+    @MinLength(7, { message: 'Password must be at least 7 characters' })
     readonly password: string;
+
+    @IsLowercase()
+    @IsEmail({}, { message: 'Must be a valid email address' })
     readonly email: string;
-    readonly age: number;
+
     readonly avatar: Buffer;
     readonly tokens: [{
         readonly token: string;
--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -1,45 +1,20 @@
 import * as mongoose from 'mongoose';
-import * as validator from 'validator';
 import * as jwt from 'jsonwebtoken';
 import * as bcrypt from 'bcrypt';
 
 export const UserSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: [true, 'User name is required'],
         trim: true,
     },
     password: {
         type: String,
-        required: [true, 'Password is required'],
         trim: true,
-        minlength: [7, 'Password must be at least 7 characters'],
-        validate(value) {
-            if (value.toLowerCase().includes('password')) {
-                throw new Error('Invalid password, cannot contain the word "password"');
-            }
-        },
     },
     email: {
         unique: true,
         type: String,
-        required: true,
         trim: true,
-        lowercase: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Invalid email');
-            }
-        },
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if (value < 0) {
-                throw new Error('Age must be a positive number');
-            }
-        },
     },
     avatar: {
         type: Buffer,
