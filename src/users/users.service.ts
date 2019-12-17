2396505cc3a807d88c027401d8789033ec7a6df2
Alan Smillie <alan.smillie@gmail.com>
2019-12-17 14:08:38 -0500

    Add find methods

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,18 +3,28 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from './interfaces/user.interface';
 import { CreateUserDto } from './dto/create-user.dto';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class UsersService {
     constructor(
-        @InjectModel('User') private readonly userModel: Model<User>) {}
-
+        @InjectModel('User') private readonly userModel: Model<User>,
+        private readonly configService: ConfigService) {}
+    // TODO: Handle error on duplicate email address
     async create(createUserDto: CreateUserDto): Promise<User> {
         const createdUser = new this.userModel(createUserDto);
         return await createdUser.save();
     }
 
-    async findAll(): Promise<User> {
+    async findAll(): Promise<User[]> {
         return await this.userModel.find().exec();
+    }
+
+    async findUserById(userId: string): Promise<User> {
+        return await this.userModel.findById(userId);
+    }
+
+    async findUserByEmail(email: string): Promise<User> {
+        return await this.userModel.findOne({ email });
     }
 }
