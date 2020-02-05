7e6ef8ab2a1240b3278dffdfcf1e9ecb2a33b72d
Alan Smillie <alan.smillie@gmail.com>
2020-02-05 14:35:58 -0500

    Change import to relative to be test-friendly

--- src/auth/passport/jwt.strategy.ts
+++ src/auth/passport/jwt.strategy.ts
@@ -1,7 +1,7 @@
 import { Injectable, Inject, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, ExtractJwt } from 'passport-jwt';
-import { UsersService } from 'src/users/users.service';
+import { UsersService } from '../../users/users.service';
 import { User } from 'src/users/interfaces/user.interface';
 
 @Injectable()
