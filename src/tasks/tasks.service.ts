61fb789b76c11324b284089571354b1a6fc08878
Alan Smillie <alan.smillie@gmail.com>
2020-02-06 14:00:25 -0500

    Optimize Imports

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Task } from './interfaces/task.interface';
