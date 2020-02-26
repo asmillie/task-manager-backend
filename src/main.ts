f4ee64ee00ed5a42a44b257ef7a2b2d1967d717f
Alan Smillie <alan.smillie@gmail.com>
2020-02-26 12:29:02 -0500

    Add Helmet Package

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import { AppModule } from './app.module';
 import { ValidationPipe, Logger } from '@nestjs/common';
 import { MongoExceptionFilter } from './mongo-exception-filter';
 import * as config from 'config';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted: true,
