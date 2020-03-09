6c5a25a5317c3a6714e64f36d804913d06d507fd
Alan Smillie <alan.smillie@gmail.com>
2020-03-09 10:06:19 -0400

    Set ENV VARS for deployment to heroku

--- config/development.ts
+++ config/development.ts
@@ -2,9 +2,6 @@ export default {
     port: 3000,
     database: {
         uri: 'mongodb://127.0.0.1:27017/task-manager-api',
-        host: '127.0.0.1',
-        port: 27017,
-        name: 'task-manager-api',
     },
     jwt: {
         secret: 'developmentsecret32',
