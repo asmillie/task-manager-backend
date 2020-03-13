fc95832ae5a3f8290b3277a658708afdc89d1e7c
Alan Smillie <alan.smillie@gmail.com>
2020-03-13 09:25:52 -0400

    Import UsersService

--- src/signup/signup.module.ts
+++ src/signup/signup.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { SignupService } from './signup.service';
 import { SignupController } from './signup.controller';
+import { UsersService } from '../users/users.service';
 
 @Module({
-  providers: [SignupService],
-  controllers: [SignupController]
+  providers: [
+    SignupService,
+    UsersService,
+  ],
+  controllers: [SignupController],
 })
 export class SignupModule {}
