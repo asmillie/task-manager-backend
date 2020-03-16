551afd8ac8267532c9b6cea37f17238aaed866a6
Alan Smillie <alan.smillie@gmail.com>
2020-03-16 09:51:10 -0400

    Create Endpoints for Email Verification

--- src/signup/signup.controller.ts
+++ src/signup/signup.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Query } from '@nestjs/common';
+import { CreateUserDto } from '../users/dto/create-user.dto';
+import { SignupService } from './signup.service';
 
 @Controller('signup')
-export class SignupController {}
+export class SignupController {
+
+    constructor(private readonly signupService: SignupService) {}
+
+    @Post()
+    async signup(createUserDto: CreateUserDto) {
+        return await this.signupService.signup(createUserDto);
+    }
+
+    @Post('verifyEmail')
+    async verifyEmail(@Query() hash: string) {
+        // Call Service
+    }
+}
