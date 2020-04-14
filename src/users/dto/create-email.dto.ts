2153f00bcddeae2f7e9866d40b0f6f0a509e3625
Alan Smillie <alan.smillie@gmail.com>
2020-04-14 11:03:01 -0400

    Flag verification as optional

--- src/users/dto/create-email.dto.ts
+++ src/users/dto/create-email.dto.ts
@@ -1,11 +1,12 @@
-import { IsLowercase, IsEmail } from 'class-validator';
+import { IsLowercase, IsEmail, IsOptional } from 'class-validator';
 
 export class CreateEmailDto {
     @IsLowercase()
     @IsEmail({}, { message: 'Must be a valid email address' })
     readonly address: string;
 
-    readonly verification: {
+    @IsOptional()
+    readonly verification?: {
         readonly code: string;
         readonly expiry: Date;
     };
