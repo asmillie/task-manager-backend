e0f49de89b81fc65bf745af88cf3d87e230577f5
Alan Smillie <alan.smillie@gmail.com>
2020-05-12 09:26:27 -0400

    Set local db uri

--- config/development.ts
+++ config/development.ts
@@ -2,8 +2,8 @@ export default {
     base_url: 'http://localhost',
     port: 3000,
     database: {
-        uri: 'mongodb+srv://taskapp:eBdlFPPgP8X9FRij@cluster0-opwul.mongodb.net/task-api?retryWrites=true',
-        // 'mongodb://127.0.0.1:27017/task-manager-api',
+        // uri: 'mongodb+srv://taskapp:eBdlFPPgP8X9FRij@cluster0-opwul.mongodb.net/task-api?retryWrites=true',
+        uri: 'mongodb://127.0.0.1:27017/task-manager-api',
     },
     jwt: {
         secret: 'developmentsecret32',
