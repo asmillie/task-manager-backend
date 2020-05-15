d2b44c85904f6db9b2353b47f3b7dd09f3c7d45c
Alan Smillie <alan.smillie@gmail.com>
2020-05-15 09:36:49 -0400

    Add comments

--- src/auth/token-ownership.guard.ts
+++ src/auth/token-ownership.guard.ts
@@ -1,6 +1,10 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Observable } from 'rxjs';
 
+/**
+ * Verifies that the JWT included in the request
+ * belongs to the User making the request.
+ */
 @Injectable()
 export class TokenOwnershipGuard implements CanActivate {
   canActivate(
