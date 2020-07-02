244ff444746a3b70c890031c762f4f3e18abc13d
Alan Smillie <alan.smillie@gmail.com>
2020-07-02 10:08:23 -0400

    Set address as a required field

--- src/users/dto/update-email.dto.ts
+++ src/users/dto/update-email.dto.ts
@@ -1,8 +1,7 @@
 import { IsLowercase, IsEmail, IsOptional } from 'class-validator';
 
 export class UpdateEmailDto {
-    @IsOptional()
     @IsLowercase()
     @IsEmail({}, { message: 'Must be a valid email address' })
-    readonly address?: string;
+    readonly address: string;
 }
