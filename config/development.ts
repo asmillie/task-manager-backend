0eecee6e4f1d3c79498b706dc8eb3bd272be5fa8
Alan Smillie <alan.smillie@gmail.com>
2020-10-23 12:04:08 -0400

    Add config vars

---
+++ config/development.ts
@@ -0,0 +1,15 @@
+export default {
+    base_url: 'http://localhost',
+    port: 3000,
+    database: {
+        // uri: 'mongodb+srv://taskapp:eBdlFPPgP8X9FRij@cluster0-opwul.mongodb.net/task-api?retryWrites=true',
+        uri: 'mongodb://127.0.0.1:27017/task-manager-api',
+    },
+    jwt: {
+        secret: 'developmentsecret32',
+        expiresIn: 3600,
+    },
+    sendgrid: {
+        key: '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',
+    },
+};
---
+++ config/test.ts
@@ -0,0 +1,10 @@
+export default {
+    base_url: 'http://localhost',
+    sendgrid: {
+        key: 'sendgrid-api-key',
+    },
+    jwt: {
+        secret: 'test-jwt-secret',
+        expiresIn: 3600,
+    },
+};
