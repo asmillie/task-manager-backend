ef874e91b1d84c19ea51ffd476546ed3e07e6140
Alan Smillie <48022778+asmillie@users.noreply.github.com>
2021-04-14 15:35:23 -0400

    Get JWT Secret from Auth0

--- src/auth/passport/jwt.strategy.ts
+++ src/auth/passport/jwt.strategy.ts
@@ -1,18 +1,26 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, ExtractJwt } from 'passport-jwt';
+import * as jwksClient from 'jwks-rsa';
 import { UsersService } from '../../users/users.service';
 import { User } from 'src/users/interfaces/user.interface';
 import * as config from 'config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-
     constructor(private readonly usersService: UsersService) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             ignoreExpiration: false,
-            secretOrKey: config.get<string>('jwt.secret'),
+            secretOrKeyProvider: jwksClient.passportJwtSecret({
+                cache: true,
+                rateLimit: true,
+                jwksRequestsPerMinute: 5,
+                jwksUri: 'https://dev-x4xgby3m.us.auth0.com/.well-known/jwks.json'
+            }),
+            algorithms: ['RS256'],
+            audience: 'task-manager-api',
+            issuer: 'https://dev-x4xgby3m.us.auth0.com/'
         });
     }
 
