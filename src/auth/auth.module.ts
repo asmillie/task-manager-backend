ded493ecc088404256af6fca78d516e3d5a79c83
Alan Smillie <48022778+asmillie@users.noreply.github.com>
2021-11-25 15:05:28 -0500

    Remove JWT Module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,20 +2,12 @@ import { HttpModule, Module } from '@nestjs/common';
 import { UsersModule } from '../users/users.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './passport/jwt.strategy';
-import { JwtModule } from '@nestjs/jwt';
 import { Auth0Service } from './auth0/auth0.service';
-import * as config from 'config';
 
 @Module({
   imports: [
     UsersModule,
     PassportModule.register({ defaultStrategy: 'jwt' }),
-    JwtModule.register({
-      secret: config.get<string>('jwt.secret'),
-      signOptions: {
-        expiresIn: config.get<number>('jwt.expiresIn'),
-      },
-    }),
     HttpModule,
   ],
   providers: [
