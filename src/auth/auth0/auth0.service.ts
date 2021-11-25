3d27bae11c694d71d5be4624315ac921475b7fae
Alan Smillie <48022778+asmillie@users.noreply.github.com>
2021-11-25 14:53:40 -0500

    Add method to request user profile from Auth0 API

--- src/auth/auth0/auth0.service.ts
+++ src/auth/auth0/auth0.service.ts
@@ -1,14 +1,31 @@
 import { HttpService, Injectable } from '@nestjs/common';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import * as config from 'config';
+
+interface IUserInfoResponse {
+    name: string;
+    email: string;
+}
 
 @Injectable()
 export class Auth0Service {
-    constructor(private httpService: HttpService) {}
+    private domain;
 
+    constructor(private httpService: HttpService) {
+        this.domain = config.get<string>('auth0.domain');
+    }
 
-    public getUserInfo$(token: string): Observable<User> {
-        
-        
-        return;
+    public getUserInfo$(token: string): Observable<any> {  
+        const userInfo = config.get<string>('auth0.endpoints.userinfo');
+        return this.httpService
+            .get<IUserInfoResponse>(`https://${this.domain}/${userInfo}`, {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            })
+            .pipe(
+                tap(res => console.log(res))
+            );
     }
 }
