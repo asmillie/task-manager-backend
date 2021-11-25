dde2eb28c897b3de66a04dd2b5799648ebc2040d
Alan Smillie <48022778+asmillie@users.noreply.github.com>
2021-11-25 12:42:57 -0500

    Add Auth0 API Config

--- config/development.ts
+++ config/development.ts
@@ -4,4 +4,10 @@ export default {
     database: {
         uri: 'mongodb://127.0.0.1:27017/task-manager-api',
     },
+    auth0: {
+        domain: 'dev-x4xgby3m.us.auth0.com',
+        endpoints: {
+            userinfo: 'userinfo'
+        }
+    }
 };
--- config/production.ts
+++ config/production.ts
@@ -4,4 +4,10 @@ export default {
     database: {
         uri: process.env.DATABASE_URI,
     },
+    auth0: {
+        domain: process.env.AUTH0_DOMAIN,
+        endpoints: {
+            userinfo: 'userinfo'
+        }
+    }
 };
