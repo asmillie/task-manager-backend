1d356dce144cd15c6f4c21c93467d605032df12f
Alan Smillie <48022778+asmillie@users.noreply.github.com>
2021-11-25 12:44:02 -0500

    Return jwt after validation

--- src/auth/passport/jwt.strategy.ts
+++ src/auth/passport/jwt.strategy.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, ExtractJwt } from 'passport-jwt';
 import * as jwksClient from 'jwks-rsa';
 import { UsersService } from '../../users/users.service';
+import { User } from 'src/users/interfaces/user.interface';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -20,13 +21,18 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
             algorithms: ['RS256'],
             audience: 'task-manager',
             issuer: 'https://dev-x4xgby3m.us.auth0.com/',
+            jsonWebTokenOptions: {
+                complete: true
+            }
         });
     }
 
     /**
-     * Called after request is authenticated, no need to do anything here.
+     * Finds and returns user that matches id contained in JWT payload.
+     * @param request Request object passed from Passport-JWT
+     * @returns Validated JSON Web Token recieved as part of request
      */
-    async validate(sub: any): Promise<Boolean> {
-        return true;
+    async validate(request: any): Promise<string> {
+        return request.signature;
     }
 }
