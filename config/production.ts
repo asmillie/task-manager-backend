272920315c8698216bf7458b435dc17618869866
Alan Smillie <48022778+asmillie@users.noreply.github.com>
2021-11-30 12:29:26 -0500

    Remove unused env vars

--- config/production.ts
+++ config/production.ts
@@ -3,11 +3,5 @@ export default {
     port: parseInt(process.env.PORT, 10),
     database: {
         uri: process.env.DATABASE_URI,
-    },
-    auth0: {
-        domain: process.env.AUTH0_DOMAIN,
-        endpoints: {
-            userinfo: 'userinfo'
-        }
     }
 };
