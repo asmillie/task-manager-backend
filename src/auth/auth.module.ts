86cbc44f6e64d27f20347e084bbb7da91a8b3c37
Alan Smillie <48022778+asmillie@users.noreply.github.com>
2021-11-30 12:31:06 -0500

    Remove unused service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,7 +2,6 @@ import { HttpModule, Module } from '@nestjs/common';
 import { UsersModule } from '../users/users.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './passport/jwt.strategy';
-import { Auth0Service } from './auth0/auth0.service';
 
 @Module({
   imports: [
@@ -12,9 +11,8 @@ import { Auth0Service } from './auth0/auth0.service';
   ],
   providers: [
     JwtStrategy,
-    Auth0Service,
   ],
   controllers: [],
-  exports: [Auth0Service],
+  exports: [],
 })
 export class AuthModule {}
