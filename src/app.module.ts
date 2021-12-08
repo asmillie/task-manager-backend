80144c27e9f7728b2398250f92d9ee333c927cdc
Alan Smillie <48022778+asmillie@users.noreply.github.com>
2021-12-08 11:08:41 -0500

    Remove unused import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { CacheModule, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 import { MongooseModule } from '@nestjs/mongoose';
 
