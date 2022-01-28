ee02df0520c759022f66e9831ac3a653cc33a597
Alan Smillie <48022778+asmillie@users.noreply.github.com>
2022-01-28 12:10:20 -0500

    Add Winston and UUID packages

--- src/logs/logger/logger.service.ts
+++ src/logs/logger/logger.service.ts
@@ -1,4 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { InjectConnection } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
+import winston from 'winston';
+import 'winston-mongodb';
 
 @Injectable()
-export class LoggerService {}
+export class LoggerService {
+    private logger: winston.Logger;
+
+    constructor(@InjectConnection() private connection: Connection) {
+        this.createLogger();
+    }
+
+    getLogger() {
+        return this.logger;
+    }
+
+    private createLogger() {
+        this.logger = winston.createLogger({
+            level: 'info',
+            format: winston.format.json(),
+            transports: [
+                new winston.transports.MongoDB({
+                    db: this.connection.asPromise()
+                })
+            ]
+        })
+
+        if (process.env.NODE_ENV !== 'production') {
+            this.logger.add(new winston.transports.Console({
+                format: winston.format.simple(),
+            }))
+        }
+    }
+}
