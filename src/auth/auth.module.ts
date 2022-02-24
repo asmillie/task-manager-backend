95a758f09574d456562bed742cb40a70aa623e54
Alan Smillie <48022778+asmillie@users.noreply.github.com>
2022-02-24 11:27:11 -0500

    Remove unused provider

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,16 +1,13 @@
-import { HttpModule, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './passport/jwt.strategy';
-import { Auth0Service } from './auth0/auth0.service';
 
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt' }),
-    HttpModule,
   ],
   providers: [
     JwtStrategy,
-    Auth0Service,
   ],
   controllers: [],
   exports: [],
