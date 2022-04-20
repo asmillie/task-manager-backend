f61fad8343c5d6d8353588f031ba6bf9c675ef9a
Alan Smillie <48022778+asmillie@users.noreply.github.com>
2022-04-20 12:34:19 -0400

    Remove unused import

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -4,12 +4,10 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { TasksService } from './tasks.service';
 import { TasksController } from './tasks.controller';
 import { TaskSchema } from './schemas/task.schema';
-import { UsersModule } from '../users/users.module';
 import { LoggerService } from '../logs/logger/logger.service';
 
 @Module({
-  imports: [ 
-      UsersModule,     
+  imports: [     
       MongooseModule.forFeature([
           { name: 'Task', schema: TaskSchema },
       ]),
